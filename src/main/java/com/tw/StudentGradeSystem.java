cdb2d85fb4c1782ba5cea9e148d16dc030358f4e
高语越 <gaoyuyue@outlook.com>
2019-05-25 08:59:16 +0800

    should return that grade infos when find some student by ids

--- src/main/java/com/tw/StudentGradeSystem.java
+++ src/main/java/com/tw/StudentGradeSystem.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StudentGradeSystem {
     private final static String STUDENT_GRADE_INFO_FORMAT_REGEX = "([^，]+)，(\\w+)，语文：(\\d+)，数学：(\\d+)，英语：(\\d+)，编程：(\\d+)";
@@ -27,5 +28,9 @@ public class StudentGradeSystem {
             return false;
         }
         return studentGradeInfos.add(studentGradeInfo);
+    }
+
+    public List<StudentGradeInfo> find(List<String> ids) {
+        return studentGradeInfos.stream().filter(item -> ids.contains(item.getStudentId())).collect(Collectors.toList());
     }
 }
--- src/test/java/com/tw/StudentGradeSystemTest.java
+++ src/test/java/com/tw/StudentGradeSystemTest.java
@@ -4,6 +4,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class StudentGradeSystemTest {
     private StudentGradeSystem studentGradeSystem;
     @Before
@@ -37,5 +42,21 @@ public class StudentGradeSystemTest {
         StudentGradeInfo studentGradeInfo = new StudentGradeInfo("张三", "201514620108", 75, 95, 80, 80);
         Assert.assertTrue(studentGradeSystem.save(studentGradeInfo));
         Assert.assertFalse(studentGradeSystem.save(studentGradeInfo));
+    }
+
+    @Test
+    public void should_return_that_grade_infos_when_find_some_student_by_ids() {
+        List<StudentGradeInfo> expectInfos = new ArrayList<>();
+        StudentGradeInfo studentGradeInfo1 = new StudentGradeInfo("张三", "201514620107", 75, 95, 80, 80);
+        StudentGradeInfo studentGradeInfo2 = new StudentGradeInfo("张三", "201514620108", 75, 95, 80, 80);
+        StudentGradeInfo studentGradeInfo3 = new StudentGradeInfo("张三", "201514620109", 75, 95, 80, 80);
+        studentGradeSystem.save(studentGradeInfo1);
+        studentGradeSystem.save(studentGradeInfo2);
+        studentGradeSystem.save(studentGradeInfo3);
+        expectInfos.add(studentGradeInfo1);
+        expectInfos.add(studentGradeInfo3);
+
+        List<StudentGradeInfo> actualInfos = studentGradeSystem.find(Arrays.asList("201514620107", "201514620109"));
+        Assert.assertArrayEquals(expectInfos.toArray(), actualInfos.toArray());
     }
 }
